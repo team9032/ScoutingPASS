13732fdf424c68c180a51b8be8cbf9ee0ed0d764
kumathavi <avi.kumath@gmail.com>
2026-03-06 19:20:22 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -235,30 +235,23 @@ var config_data = `
       "code": "pen",
       "type": "bool"
     },
-    { "name": "Fuel Percentage",
-      "tooltip": "What percentage of the total fuel for this alliance did this robot score?",
-      "code": "pct",
-      "type": "number",
-      "min": 0,
-      "max": 100
-    },
     { "name": "Comments On-Cycle",
       "code": "con",
       "type": "text",
       "size": 15,
-      "maxSize": 55
+      "maxSize": 500
     }
     { "name": "Comments Off-Cycle",
       "code": "cof",
       "type": "text",
       "size": 15,
-      "maxSize": 55
+      "maxSize": 500
     }
     { "name": "Comments",
       "code": "co",
       "type": "text",
       "size": 15,
-      "maxSize": 55
+      "maxSize": 500
     }
   ]
 }`;
